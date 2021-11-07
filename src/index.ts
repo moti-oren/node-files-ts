38576bd215356a561af70e1b8de7d2dc7cf31f52
moti-oren <motioren3@gmail.com>
2021-11-07 13:15:45 +0200

    index.ts content added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import Search from './lib/Search';
-import * as filters from './utils/filters';
+import filters from './utils/filters';
 
+const { filterByName, filterByDate } = filters;
+
+let startDate: Date = new Date(2018, 8, 16), endDate: Date = new Date(2020, 5, 23);
+const finderByDate = new Search('C:/Users/user', filterByDate(startDate, endDate));
+const finderByName = new Search('C:/Users/user', filterByName('my-file', '.ext'));
+
+finderByName.on('found', function (path) {
+    console.info('Match found:', path);
+});
+
+finderByName.on('complete', function () {
+    console.info('Search complete!');
+});
+
+finderByName.startSearch();
